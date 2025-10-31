4734827e877bd409372fc1cee576e47793e4debf
qssq <2665339398@qq.com>
2025-10-31 17:20:50 +0800

    feat: 添加存储类型设置到数据库初始化脚本

--- blog-backend/sql/init.sql
+++ blog-backend/sql/init.sql
@@ -475,7 +475,8 @@ INSERT INTO settings (key, value, type, "group", label, created_at, updated_at)
 VALUES 
 ('site_name', '我的博客', 'text', 'site', '网站名称', NOW(), NOW()),
 ('site_icp', '', 'text', 'site', 'ICP备案号', NOW(), NOW()),
-('site_police', '', 'text', 'site', '公安备案号', NOW(), NOW())
+('site_police', '', 'text', 'site', '公安备案号', NOW(), NOW()),
+('storage_type', 'local', 'text', 'upload', '存储类型', NOW(), NOW())
 ON CONFLICT (key) DO NOTHING;
 
 -- 插入最近7天的访问统计初始记录
