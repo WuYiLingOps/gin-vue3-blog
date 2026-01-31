c16eb88460805ed3f37b5ac4279a47f15b9a0dda
無以菱 <huangjing510@126.com>
2026-01-31 16:51:29 +0800

    docs: 为密码加密工具函数添加头部注释和代码注释

--- blog-backend/util/password.go
+++ blog-backend/util/password.go
@@ -1,3 +1,13 @@
+/*
+ * 项目名称：blog-backend
+ * 文件名称：password.go
+ * 创建时间：2026-01-31 16:41:24
+ *
+ * 系统用户：Administrator
+ * 作　　者：無以菱
+ * 联系邮箱：huangjing510@126.com
+ * 功能描述：密码加密工具函数，提供密码哈希和验证功能
+ */
 package util
 
 import (
@@ -15,4 +25,3 @@ func CheckPassword(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
-
