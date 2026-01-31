dacbf4d3c52b1b2d2bec5d8f39d7050cb92ab75b
無以菱 <huangjing510@126.com>
2026-01-31 16:52:18 +0800

    docs: 为日历处理器添加头部注释和代码注释

--- blog-backend/handler/calendar.go
+++ blog-backend/handler/calendar.go
@@ -1,3 +1,13 @@
+/*
+ * 项目名称：blog-backend
+ * 文件名称：calendar.go
+ * 创建时间：2026-01-31 16:05:15
+ *
+ * 系统用户：Administrator
+ * 作　　者：無以菱
+ * 联系邮箱：huangjing510@126.com
+ * 功能描述：日历处理器，提供Gitee贡献热力图数据查询功能
+ */
 package handler
 
 import (
@@ -7,10 +17,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// CalendarHandler 日历处理器结构体
 type CalendarHandler struct {
 	service *service.CalendarService
 }
 
+// NewCalendarHandler 创建日历处理器实例
 func NewCalendarHandler() *CalendarHandler {
 	return &CalendarHandler{
 		service: service.NewCalendarService(),
