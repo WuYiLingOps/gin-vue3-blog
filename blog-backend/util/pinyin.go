4c5a0c061fdb4bb573aa3ae82da69a02cedeb327
無以菱 <huangjing510@126.com>
2026-01-31 16:51:29 +0800

    docs: 为拼音转换工具函数添加头部注释和代码注释

--- blog-backend/util/pinyin.go
+++ blog-backend/util/pinyin.go
@@ -1,3 +1,13 @@
+/*
+ * 项目名称：blog-backend
+ * 文件名称：pinyin.go
+ * 创建时间：2026-01-31 16:41:24
+ *
+ * 系统用户：Administrator
+ * 作　　者：無以菱
+ * 联系邮箱：huangjing510@126.com
+ * 功能描述：拼音转换工具函数，提供中文转拼音生成URL友好的slug功能
+ */
 package util
 
 import (
