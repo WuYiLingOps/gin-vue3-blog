9fa52a3e3bf652986350e72fe71c91760f9f6e56
無以菱 <huangjing510@126.com>
2026-01-31 16:52:05 +0800

    docs: 为IP上下文中间件添加头部注释和代码注释

--- blog-backend/middleware/ip_context.go
+++ blog-backend/middleware/ip_context.go
@@ -1,3 +1,13 @@
+/*
+ * 项目名称：blog-backend
+ * 文件名称：ip_context.go
+ * 创建时间：2026-01-31 16:17:29
+ *
+ * 系统用户：Administrator
+ * 作　　者：無以菱
+ * 联系邮箱：huangjing510@126.com
+ * 功能描述：IP上下文中间件，将客户端IP存入请求上下文，供后续中间件和处理器使用
+ */
 package middleware
 
 import (
@@ -7,7 +17,9 @@ import (
 )
 
 // IPContextMiddleware IP上下文中间件
-// 将获取到的客户端IP存入请求上下文，供后续中间件和处理器使用
+// 功能说明：将获取到的客户端IP存入请求上下文，供后续中间件和处理器使用
+// 返回:
+//   - gin.HandlerFunc: Gin中间件处理函数
 func IPContextMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 获取客户端IP
