a8a13faba4e31290946633e6c7b8f5491f10f5e5
無以菱 <huangjing510@126.com>
2026-01-31 16:52:21 +0800

    docs: 为友链分类处理器添加头部注释和代码注释

--- blog-backend/handler/friendlink_category.go
+++ blog-backend/handler/friendlink_category.go
@@ -1,3 +1,13 @@
+/*
+ * 项目名称：blog-backend
+ * 文件名称：friendlink_category.go
+ * 创建时间：2026-01-31 16:05:15
+ *
+ * 系统用户：Administrator
+ * 作　　者：無以菱
+ * 联系邮箱：huangjing510@126.com
+ * 功能描述：友链分类管理处理器，提供友链分类的增删改查功能
+ */
 package handler
 
 import (
@@ -9,10 +19,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// FriendLinkCategoryHandler 友链分类处理器结构体
 type FriendLinkCategoryHandler struct {
 	service *service.FriendLinkCategoryService
 }
 
+// NewFriendLinkCategoryHandler 创建友链分类处理器实例
 func NewFriendLinkCategoryHandler() *FriendLinkCategoryHandler {
 	return &FriendLinkCategoryHandler{
 		service: service.NewFriendLinkCategoryService(),
