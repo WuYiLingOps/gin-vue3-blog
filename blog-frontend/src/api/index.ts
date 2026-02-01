896cd58094f570bce914affaa7a3d16178335927
無以菱 <huangjing510@126.com>
2026-02-01 20:02:14 +0800

    docs: 为API导出文件添加头部注释

--- blog-frontend/src/api/index.ts
+++ blog-frontend/src/api/index.ts
@@ -1,4 +1,13 @@
-// API 统一导出
+/*
+ * 项目名称：blog-frontend
+ * 文件名称：index.ts
+ * 创建时间：2026-02-01 19:52:47
+ *
+ * 系统用户：Administrator
+ * 作　　者：無以菱
+ * 联系邮箱：huangjing510@126.com
+ * 功能描述：API 模块统一导出文件，集中导出所有 API 接口，方便其他模块统一导入使用。
+ */
 
 export * from './auth'
 export * from './post'
