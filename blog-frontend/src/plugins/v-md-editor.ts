ec18dbf52e48e85b4d2b1d749b8c58259a74aa81
無以菱 <huangjing510@126.com>
2026-02-08 00:03:44 +0800

    chore: 提取Markdown编辑器插件全局初始化逻辑

---
+++ blog-frontend/src/plugins/v-md-editor.ts
@@ -0,0 +1,38 @@
+import VMdEditor from '@kangc/v-md-editor'
+import VMdPreview from '@kangc/v-md-editor/lib/preview'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import '@kangc/v-md-editor/lib/style/preview.css'
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+import Prism from 'prismjs'
+
+// 引入代码高亮语言包
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-c'
+import 'prismjs/components/prism-cpp'
+import 'prismjs/components/prism-sql'
+import 'prismjs/components/prism-yaml'
+import 'prismjs/components/prism-markdown'
+import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-scss'
+
+// 配置配置项
+const config = {
+  Prism,
+  codeHighlightExtensionMap: {
+    vue: 'html',
+  }
+}
+
+// 配置编辑器主题
+VMdEditor.use(vuepressTheme, config)
+// 配置预览组件主题
+VMdPreview.use(vuepressTheme, config)
+
+export { VMdEditor, VMdPreview }
