53acdfaf3d1423b825721bf35deb155006cbb363
無以菱 <huangjing510@126.com>
2026-02-08 00:03:40 +0800

    style: 优化代码块样式，增加圆角并居中显示

--- blog-frontend/src/components/CommentContent.vue
+++ blog-frontend/src/components/CommentContent.vue
@@ -18,31 +18,6 @@
 <script setup lang="ts">
 import { ref, onMounted, watch, nextTick } from 'vue'
 import { useMessage } from 'naive-ui'
-import VMdPreview from '@kangc/v-md-editor/lib/preview'
-import '@kangc/v-md-editor/lib/style/preview.css'
-import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
-import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
-import Prism from 'prismjs'
-
-// 引入代码高亮语言包（精简版）
-import 'prismjs/components/prism-json'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-go'
-import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-sql'
-import 'prismjs/components/prism-markdown'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-yaml'
-
-// 配置主题
-VMdPreview.use(vuepressTheme, {
-  Prism,
-  codeHighlightExtensionMap: {
-    vue: 'html',
-  }
-})
 
 interface Props {
   content: string
